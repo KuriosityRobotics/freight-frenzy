f92327475bd3d670c2a7925ae2fbb3b43d76352a
chop0 <chop@quenda.net>
2021-08-21 15:43:36 -0700

    made it more functional lmao

---
+++ TeamCode/src/main/java/com/kuriosityrobotics/firstforward/robot/sensors/OdoProcessing.java
@@ -0,0 +1,13 @@
+package com.kuriosityrobotics.firstforward.robot.sensors;
+
+import com.kuriosityrobotics.firstforward.robot.Robot;
+
+public class OdoProcessing {
+    private final Robot robot;
+
+    public OdoProcessing(Robot robot) {
+        this.robot = robot;
+    }
+
+    void process(OdoSensorData data) {}
+}
