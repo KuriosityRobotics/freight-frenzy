fdd159635e464d51437d692dfcd686f6e56fb5bb
Ethan Kou <bluecheetos666@gmail.com>
2021-08-31 21:54:58 -0700

    simple drivetrain controls (front, back, strafe, turn)

---
+++ TeamCode/src/main/java/com/kuriosityrobotics/firstforward/robot/modules/DrivetrainModule.java
@@ -0,0 +1,98 @@
+package com.kuriosityrobotics.firstforward.robot.modules;
+import com.kuriosityrobotics.firstforward.robot.Robot;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import java.util.ArrayList;
+
+
+public class DrivetrainModule implements Module{
+    private final Robot robot;
+    private final Boolean isOn;
+
+    //public TelemetryDump telemetryDump;
+    //states
+    public double strafe = 0;
+    public double forwards = 0;
+    public double turn = 0;
+
+    //motors
+    private DcMotor fLeft;
+    private DcMotor fRight;
+    private DcMotor bLeft;
+    private DcMotor bRight;
+
+    public DrivetrainModule(Robot robot, boolean isOn) {
+        //robot.telemetryDump.registerProvider(this);
+        this.robot = robot;
+        this.isOn = isOn;
+    }
+
+    //update motor power
+    public void update() {
+        double fLPower = forwards + turn + strafe;
+        double fRPower = forwards - turn - strafe;
+        double bLPower = forwards + turn - strafe;
+        double bRPower = forwards - turn + strafe;
+
+        double scale = scaleDown(fLPower, fRPower, bLPower, bRPower);
+
+        fLPower *= scale;
+        fRPower *= scale;
+        bLPower *= scale;
+        bRPower *= scale;
+
+        setMotorPowers(fLPower, fRPower, bLPower, bRPower);
+    }
+
+    private void setMotorPowers(double fLPower, double fRPower, double bLPower, double bRPower) {
+        setMotorPower(fLeft, fLPower);
+        setMotorPower(fRight, fRPower);
+        setMotorPower(bLeft, bLPower);
+        setMotorPower(bRight, bRPower);
+    }
+
+    private void setMotorPower(DcMotor motor, double power) {
+        if (Math.abs(power) < 0.06) {
+            motor.setPower(0);
+        } else {
+            motor.setPower(power);
+        }
+    }
+
+    //scale down motor power so largest/smallest is 1/-1
+    public double scaleDown(double a, double b, double c, double d){
+        double max = Math.max(Math.abs(d), Math.max(Math.abs(c), Math.max(Math.abs(a), Math.abs(b))));
+        if (max < 1) {return 1;}
+        return max;
+    }
+
+    public boolean isOn() {
+        return isOn;
+    }
+
+    //@Override
+    public ArrayList<String> getTelemetryData() {
+        ArrayList<String> data = new ArrayList<>();
+        data.add("forwards movement: " + forwards);
+        data.add("strafe movement: " + strafe);
+        data.add("turn movement: " + turn);
+        return data;
+    }
+
+    public String getName() {
+        return "DriveTrainModule";
+    }
+
+    public void initModules() {
+        fLeft = robot.getDcMotor("fLeft");
+        fRight = robot.getDcMotor("fRight");
+        bLeft = robot.getDcMotor("bLeft");
+        bRight = robot.getDcMotor("bRight");
+
+        fLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+        fRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        bLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+        bRight.setDirection(DcMotorSimple.Direction.REVERSE);
+    }
+}
