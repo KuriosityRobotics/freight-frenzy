fa59f02281e1cff0c3d38c2928b13e9cff27237e
sharkree <56747474+sharkree@users.noreply.github.com>
2021-10-31 19:42:06 -0700

    FF-28 FF-29: Fixed up error catching in VisionTest

--- TeamCode/src/main/java/com/kuriosityrobotics/firstforward/robot/opmodes/tests/VisionTest.java
+++ TeamCode/src/main/java/com/kuriosityrobotics/firstforward/robot/opmodes/tests/VisionTest.java
@@ -1,11 +1,10 @@
 package com.kuriosityrobotics.firstforward.robot.opmodes.tests;
 
 import com.kuriosityrobotics.firstforward.robot.Robot;
-//import com.kuriosityrobotics.firstforward.robot.vision.WebcamLocalization;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import javassist.NotFoundException;
+//import com.kuriosityrobotics.firstforward.robot.vision.WebcamLocalization;
 
 @TeleOp
 public class VisionTest extends LinearOpMode {
@@ -15,7 +14,7 @@ public class VisionTest extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         try {
             robot = new Robot(hardwareMap, telemetry, this);
-        } catch (NotFoundException e) {
+        } catch (Exception e) {
             this.stop();
             throw new RuntimeException(e);
         }
