29ef9158c4981bc17db1a49197ecf7ee060e1e0b
sharkree <56747474+sharkree@users.noreply.github.com>
2021-11-22 17:41:00 -0800

    FF-24: Fixed issues and made Filedump actually stop when the program isn't running

--- TeamCode/src/main/java/com/kuriosityrobotics/firstforward/robot/debug/FileDump.java
+++ TeamCode/src/main/java/com/kuriosityrobotics/firstforward/robot/debug/FileDump.java
@@ -114,4 +114,13 @@ public class FileDump {
                 ).collect(Collectors.joining(",")));
         }
     }
+
+    public static void close() {
+        if (activated) {
+            writer.close();
+            activated = false;
+
+            Log.i("FileDump", "Filedump has been stopped");
+        }
+    }
 }
