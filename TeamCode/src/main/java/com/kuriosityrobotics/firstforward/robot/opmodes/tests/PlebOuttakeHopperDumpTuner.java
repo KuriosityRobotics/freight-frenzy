46f3b6e2a996d639dc0584c1bcac8df14104d17d
chop@quenda.net <chop@quenda.net>
2021-11-27 01:16:04 -0800

    FF-51 outtake 2

---
+++ TeamCode/src/main/java/com/kuriosityrobotics/firstforward/robot/opmodes/tests/PlebOuttakeHopperDumpTuner.java
@@ -0,0 +1,29 @@
+package com.kuriosityrobotics.firstforward.robot.opmodes.tests;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp
+public class PlebOuttakeHopperDumpTuner extends LinearOpMode {
+    Servo dumpMotor;
+
+    double dumpMotorPos = 0.5;
+
+    @Override
+    public void runOpMode() {
+        dumpMotor = hardwareMap.get(Servo.class, "hopper_pivot_servo");
+
+        waitForStart();
+
+        dumpMotor.setPosition(dumpMotorPos);
+
+        while (opModeIsActive()) {
+            dumpMotorPos += gamepad1.right_stick_y * 0.00005;
+            dumpMotor.setPosition(dumpMotorPos);
+
+            telemetry.addData("dump motor pos: ", dumpMotorPos);
+            telemetry.update();
+        }
+    }
+}
---
+++ TeamCode/src/main/java/com/kuriosityrobotics/firstforward/robot/opmodes/tests/PlebOuttakeVerticalSlideTuner.java
@@ -0,0 +1,32 @@
+package com.kuriosityrobotics.firstforward.robot.opmodes.tests;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp
+public class PlebOuttakeVerticalSlideTuner extends LinearOpMode {
+    DcMotorEx slide;
+
+    int verticalSlideMotorPos = 0;
+
+    @Override
+    public void runOpMode() {
+        slide = (DcMotorEx) hardwareMap.get(DcMotor.class, "linear_slide_motor");
+        slide.setTargetPosition(0);
+        slide.setPower(1);
+        slide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            verticalSlideMotorPos += Math.signum(gamepad1.right_stick_y);
+            slide.setTargetPosition(verticalSlideMotorPos);
+
+            telemetry.addData("verticalSlideMotorPos: ", slide.getCurrentPosition());
+            telemetry.update();
+        }
+    }
+}
