72b68c2da43752d8f8e1c9e47da6bc0921bd8bd2
Daniel Kou <daniel.j.kou@gmail.com>
2021-11-27 19:37:02 -0800

    Detection progress

--- TeamCode/src/main/java/com/kuriosityrobotics/firstforward/robot/opmodes/tests/PlebDetect.java
+++ TeamCode/src/main/java/com/kuriosityrobotics/firstforward/robot/opmodes/tests/PlebDetect.java
@@ -25,9 +25,8 @@ public class PlebDetect extends LinearOpMode {
 
     @Override
     public void runOpMode() {
-        WebcamName name = hardwareMap.get(WebcamName.class, "Webcam 1");
         this.detector =  new TeamMarkerDetection();
-        this.managedCamera = new ManagedCamera(name, detector);
+        this.managedCamera = new ManagedCamera("Webcam 1", hardwareMap, detector);
 
         waitForStart();
         while (opModeIsActive()) {
