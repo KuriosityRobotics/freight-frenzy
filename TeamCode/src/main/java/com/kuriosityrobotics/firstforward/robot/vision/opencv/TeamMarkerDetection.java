ced0970682320f9ad45899acc262be027ceaf800
sharkree <daniel.j.kou@gmail.com>
2022-01-15 12:31:54 -0800

    detect now only runs once(and hopefully it works

--- TeamCode/src/main/java/com/kuriosityrobotics/firstforward/robot/vision/opencv/TeamMarkerDetection.java
+++ TeamCode/src/main/java/com/kuriosityrobotics/firstforward/robot/vision/opencv/TeamMarkerDetection.java
@@ -54,41 +54,39 @@ public class TeamMarkerDetection implements OpenCvConsumer, Telemeter {
     @Override
     public void processFrame(Mat frame) {
         if (isOn) {
-            for (int i = 0; i < 10; i++) {
-                Core.rotate(frame, frame, Core.ROTATE_90_CLOCKWISE);
+            Core.rotate(frame, frame, Core.ROTATE_90_CLOCKWISE);
 
-                // width is 1920 height is 1080
-                // these are tuned, both accurate AND consistent
-                final Rect boundingBox1 = new Rect(new Point(284, 482), new Point(475, 765));
-                final Rect boundingBox2 = new Rect(new Point(684, 482), new Point(950, 765));
-                final Rect boundingBox3 = new Rect(new Point(1184, 482), new Point(1425, 765));
+            // width is 1920 height is 1080
+            // these are tuned, both accurate AND consistent
+            final Rect boundingBox1 = new Rect(new Point(284, 482), new Point(475, 765));
+            final Rect boundingBox2 = new Rect(new Point(684, 482), new Point(950, 765));
+            final Rect boundingBox3 = new Rect(new Point(1184, 482), new Point(1425, 765));
 
-                Mat submat1 = frame.submat(boundingBox1);
-                Mat submat2 = frame.submat(boundingBox2);
-                Mat submat3 = frame.submat(boundingBox3);
-                Vector3D sectionOne = new Vector3D(rgbaToRgb(Core.mean(submat1).val));
-                Vector3D sectionTwo = new Vector3D(rgbaToRgb(Core.mean(submat2).val));
-                Vector3D sectionThree = new Vector3D(rgbaToRgb(Core.mean(submat3).val));
+            Mat submat1 = frame.submat(boundingBox1);
+            Mat submat2 = frame.submat(boundingBox2);
+            Mat submat3 = frame.submat(boundingBox3);
+            Vector3D sectionOne = new Vector3D(rgbaToRgb(Core.mean(submat1).val));
+            Vector3D sectionTwo = new Vector3D(rgbaToRgb(Core.mean(submat2).val));
+            Vector3D sectionThree = new Vector3D(rgbaToRgb(Core.mean(submat3).val));
 
-                submat1.release();
-                submat2.release();
-                submat3.release();
+            submat1.release();
+            submat2.release();
+            submat3.release();
 
-                double area1 = sectionOne.crossProduct(RED).getNorm();
-                double area2 = sectionTwo.crossProduct(RED).getNorm();
-                double area3 = sectionThree.crossProduct(RED).getNorm();
+            double area1 = sectionOne.crossProduct(RED).getNorm();
+            double area2 = sectionTwo.crossProduct(RED).getNorm();
+            double area3 = sectionThree.crossProduct(RED).getNorm();
 
-                double closestToRed = new Min().evaluate(new double[]{area1, area2, area3});
-                if (closestToRed == area1) {
-                    Imgproc.rectangle(frame, boundingBox1, new Scalar(0, 0, 0), 5);
-                    this.location = TeamMarkerLocation.LEVEL_1;
-                } else if (closestToRed == area2) {
-                    Imgproc.rectangle(frame, boundingBox2, new Scalar(0, 0, 0), 5);
-                    this.location = TeamMarkerLocation.LEVEL_2;
-                } else if (closestToRed == area3) {
-                    Imgproc.rectangle(frame, boundingBox3, new Scalar(0, 0, 0), 5);
-                    this.location = TeamMarkerLocation.LEVEL_3;
-                }
+            double closestToRed = new Min().evaluate(new double[]{area1, area2, area3});
+            if (closestToRed == area1) {
+                Imgproc.rectangle(frame, boundingBox1, new Scalar(0, 0, 0), 5);
+                this.location = TeamMarkerLocation.LEVEL_1;
+            } else if (closestToRed == area2) {
+                Imgproc.rectangle(frame, boundingBox2, new Scalar(0, 0, 0), 5);
+                this.location = TeamMarkerLocation.LEVEL_2;
+            } else if (closestToRed == area3) {
+                Imgproc.rectangle(frame, boundingBox3, new Scalar(0, 0, 0), 5);
+                this.location = TeamMarkerLocation.LEVEL_3;
             }
         }
 
