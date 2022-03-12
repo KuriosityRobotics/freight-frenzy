7046b81894b658f737b8f0964f9d8a6da04fba83
Daniel Kou <dkou@Daniels-MacBook-Pro-2.local>
2022-03-11 16:18:51 -0800

    blue carousel

--- TeamCode/src/main/java/com/kuriosityrobotics/firstforward/robot/opmodes/auto/BlueCarousel.java
+++ TeamCode/src/main/java/com/kuriosityrobotics/firstforward/robot/opmodes/auto/BlueCarousel.java
@@ -1,85 +1,61 @@
 package com.kuriosityrobotics.firstforward.robot.opmodes.auto;
 
 import com.kuriosityrobotics.firstforward.robot.Robot;
-import com.kuriosityrobotics.firstforward.robot.pathfollow.Action;
+import com.kuriosityrobotics.firstforward.robot.modules.outtake.OuttakeModule;
 import com.kuriosityrobotics.firstforward.robot.pathfollow.PurePursuit;
-import com.kuriosityrobotics.firstforward.robot.pathfollow.VelocityLock;
 import com.kuriosityrobotics.firstforward.robot.pathfollow.WayPoint;
-import com.kuriosityrobotics.firstforward.robot.pathfollow.motionprofiling.MotionProfile;
+import com.kuriosityrobotics.firstforward.robot.util.math.Point;
 import com.kuriosityrobotics.firstforward.robot.util.math.Pose;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-import java.util.ArrayList;
-
 @com.qualcomm.robotcore.eventloop.opmode.Autonomous
 public class BlueCarousel extends LinearOpMode {
-    public static final Pose START = new Pose(140.5-6, 94.5, Math.toRadians(-90));
+    public static final Pose START = Pose.fieldMirror(9.5, (23.5*5)-0.5, Math.toRadians(-90));
 
-    public static final Pose CAROUSEL = new Pose(140.5-12.8-3.25, 124, Math.toRadians(68));
+    public static final Pose WOBBLE = Pose.fieldMirror(32, 111, Math.toRadians(-27));
 
-    public static final Pose WOBBLE = new Pose(140.5-31.6, 107.75, Math.toRadians(25));
+    public static final Point PRE_CAROUSEL = Point.fieldMirror(18, 118);
+    public static final Pose CAROUSEL = Pose.fieldMirror(12.8, 124, Math.toRadians(-75));
 
-    public static final Pose WALL_ENT = new Pose(10, 100, Math.toRadians(180));
-    public static final Pose WAREHOUSE_PARK = new Pose(140.5-32, 126, Math.toRadians(0));
-    public static final Pose PARK = new Pose(8, 28, Math.toRadians(180));
+    public static final Pose PARK = Pose.fieldMirror(36, 5*23.5 + 12, Math.toRadians(-90));
 
     public void runOpMode() {
         Robot robot = null;
         try {
             robot = new Robot(hardwareMap, telemetry, this);
         } catch (Exception e) {
-            stop();
+            this.stop();
             e.printStackTrace();
             return;
         }
 
-        Robot.isBlue = true;
-        Robot.isCarousel = true;
         robot.resetPose(START);
         robot.carouselModule.clockwise = true;
+        Robot.isCarousel = true;
 
         waitForStart();
 
-        ArrayList<Action> carouselActions = new ArrayList<>();
-        carouselActions.add(robot.carouselModule.carouselAction());
-        PurePursuit toCarousel = new PurePursuit(new WayPoint[]{
-                new WayPoint(START),
-                new WayPoint(START.x - 20, START.y + 4, new VelocityLock(0.5 * MotionProfile.ROBOT_MAX_VEL)),
-                new WayPoint(CAROUSEL.x, CAROUSEL.y - 7.5, CAROUSEL.heading, 4),
-                new WayPoint(CAROUSEL.x, CAROUSEL.y, CAROUSEL.heading, 0, carouselActions)
-        }, 4);
+        OuttakeModule.VerticalSlideLevel detected = robot.visionThread.getTeamMarkerDetector().getLocation().slideLevel();
 
-        ArrayList<Action> wobbleActions = new ArrayList<>();
-        ArrayList<Action> other = new ArrayList<>();
-        other.add(robot.outtakeModule.extendOuttakeAction(robot.visionThread.getTeamMarkerDetector().getLocation().slideLevel()));
-        wobbleActions.add(robot.outtakeModule.dumpOuttakeAction());
         PurePursuit toWobble = new PurePursuit(new WayPoint[]{
-                new WayPoint(CAROUSEL, other),
-                new WayPoint(CAROUSEL.between(WOBBLE), 0.3 * MotionProfile.ROBOT_MAX_VEL, new ArrayList<>()),
-                new WayPoint(WOBBLE, 0, wobbleActions)
-        }, 4);
+                new WayPoint(START, robot.outtakeModule.extendOuttakeAction(detected)),
+                new WayPoint(WOBBLE, 0, robot.outtakeModule.dumpOuttakeAction())
+        }, true, 4);
 
-//        PurePursuit toPark = new PurePursuit(new ActionExecutor(hardwareMap), new WayPoint[]{
-//                new WayPoint(CAROUSEL),
-//                new WayPoint(WALL_ENT, 0.2 * MotionProfile.ROBOT_MAX_VEL, new ArrayList<>()),
-//                new WayPoint(PARK, 0, new ArrayList<>())
-//        }, 4);
+        PurePursuit toCarousel = new PurePursuit(new WayPoint[]{
+                new WayPoint(WOBBLE),
+                new WayPoint(PRE_CAROUSEL, 10),
+                new WayPoint(CAROUSEL, 0, robot.carouselModule.carouselAction())
+        }, false, 4);
 
         PurePursuit toPark = new PurePursuit(new WayPoint[]{
                 new WayPoint(CAROUSEL),
-                new WayPoint(CAROUSEL.between(WAREHOUSE_PARK), 9, new ArrayList<>()),
-                new WayPoint(WAREHOUSE_PARK, 0, new ArrayList<>())
-        }, 4);
+                new WayPoint(PARK)
+        }, true, 4);
 
-        // DETECT the THING
-
-        // go to carousel
+        robot.followPath(toWobble);
         robot.followPath(toCarousel);
-
-        // to wobble
-//        robot.followPath(toWobble);
-//
-//        robot.followPath(toPark);
+        robot.followPath(toPark);
 
         Robot.isCarousel = false;
     }
